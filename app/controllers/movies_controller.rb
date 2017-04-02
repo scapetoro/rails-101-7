2df4e5bec2485d3e4a0ecf73ae6c5124dfce7bf7
lihantao <htl00001111@163.com>
2017-04-02 17:14:22 +0800

    use before_action to find_movie_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user! ,only: [:new, :create]
+  before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @movies = Movie.all
   end
@@ -9,7 +10,6 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def new
@@ -19,7 +19,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     @movie.user = current_user
-    
+
     if @movie.save
       redirect_to movies_path
     else
@@ -28,7 +28,6 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "Update Success"
     else
@@ -37,12 +36,19 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to movies_path, alert: "Movie Deleted"
   end
 
   private
+
+  def find_movie_and_check_permission
+    @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "You have no permition."
+    end
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :description)
